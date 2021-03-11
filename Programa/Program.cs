2f8eaaf05404e1c73a83676510fe8778c27eb549
Primi0 <62573087+Primi0@users.noreply.github.com>
2021-03-10 18:34:01 -0600

    Add files via upload

---
+++ Programa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            
+            Console.WriteLine("Enter a value for a:");
+            a = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for b:");
+            b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine($"adding {a} + {b} equals {AddNumbers(a, b)}");
+        } 
+
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
